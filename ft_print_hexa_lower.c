e995b498288da13dd5c95220d403921c00b63b5e
rmaanane <100700233+ridamaanane@users.noreply.github.com>
2024-12-08 16:16:04 +0100

    Update ft_print_hexa_lower.c

--- ft_print_hexa_lower.c
+++ ft_print_hexa_lower.c
@@ -20,11 +20,7 @@ int	ft_puthexa_l(unsigned long nb)
 	count = 0;
 	base_low = "0123456789abcdef";
 	if (nb >= 16)
-	{
-		count += ft_puthexa_l(nb / 16);
-		count += write(1, &base_low[nb % 16], 1);
-	}
-	else
-		count += write(1, &base_low[nb % 16], 1);
+		count += ft_puthexa_l(nb / 16);	
+	count += write(1, &base_low[nb % 16], 1);
 	return (count);
 }
